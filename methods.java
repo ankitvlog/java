c164ea7ac8d3b7e912a30aa2044e4312ce98d60b
ankitvlog <88242036+ankitvlog@users.noreply.github.com>
2021-07-31 23:03:55 +0530

    Created Methods.java
    This file prints methods on the screen.

---
+++ methods.java
@@ -0,0 +1,12 @@
+/* Methods in Java */
+
+class DemoMethod{
+public static void main(String[] args){
+int a = 625;
+System.out.println("Square root of"+a"is:"+Math.sqrt(a));
+myMethod();
+}
+public static void myMethod(){
+System.out.println("User define method is called");
+}
+}
